b7e774b7f31385a852e66343fd09ed7652010685
lordslair <root@template.ovh>
2017-08-02 06:16:56 -0400

    Added test script for UTM::Untappd

---
+++ docker/test/getDataFromUntappd.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib '../lib';
+use UTM::Untappd;
+
+#
+# Variables initialization
+#
+
+my $username = $ARGV[0];
+my $Country = UTM::Untappd::getData($username);
+
+foreach my $country ( sort keys %{$Country} )
+{
+    printf "%-20s | %-4d\n", $country, $Country->{$country}{'count'};
+}
