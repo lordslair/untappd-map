2fff52626a69d97dadd96bcda63990056b00e32a
lordslair <root@template.ovh>
2017-08-02 04:54:59 -0400

    Added test script

---
+++ docker/test/getStatus.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Net::Twitter::Lite::WithAPIv1_1;
+use Data::Dumper;
+
+use lib '../lib';
+use YAML::Tiny;
+
+binmode(STDOUT, ":utf8");
+
+#
+# Variables initialization
+#
+
+my $twitterfile = '../twitter-config.yaml';
+my $twittyaml = YAML::Tiny->read( $twitterfile );
+my $twitter = Net::Twitter::Lite::WithAPIv1_1->new(
+    access_token_secret => $twittyaml->[0]{AccessTokenSecret},
+    consumer_secret     => $twittyaml->[0]{ConsumerSecret},
+    access_token        => $twittyaml->[0]{AccessToken},
+    consumer_key        => $twittyaml->[0]{ConsumerKey},
+    user_agent          => 'UntappdMap Bot',
+    ssl => 1,
+);
+
+print Dumper my $status = $twitter->show_status({ id => $ARGV[0] });
