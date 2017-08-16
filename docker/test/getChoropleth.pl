486588856e591c099ef9d58755232e20ed0a0233
lordslair <rvandepo+github@gmail.com>
2017-08-16 09:09:00 -0400

    getChoropleth.pl 1.0

--- docker/test/getChoropleth.pl
+++ docker/test/getChoropleth.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use lib '../lib';
+use lib '/home/untappd-map/lib';
 use UTM::Choropleth;
 
 #
@@ -12,16 +12,16 @@ use UTM::Choropleth;
 
 my %Country;
 my $biggest     = 200;
-my $input_file  = '../data/BlankMap-World6.svg';
-my $output_file = './getChoropleth.svg';
+my $input_file  = '/home/untappd-map/data/BlankMap-World6.svg';
+my $output_file = '/home/untappd-map/data/getChoropleth.pl.svg';
 
 $Country{'France'}{'count'}  = 150;
 $Country{'France'}{'code'}   = 'fr';
 $Country{'France'}{'color'}  = 'f6cf00';
 $Country{'Belgium'}{'count'} = 10;
 $Country{'Belgium'}{'code'}  = 'be';
-$Country{'Belgium'}{'color'} = 'c05917';
+$Country{'Belgium'}{'color'}  = 'c05917';
 
-my $Country_ref              = \%Country;
+my $Country_ref         = \%Country;
 
 UTM::Choropleth::Draw($Country_ref,$input_file,$output_file);
