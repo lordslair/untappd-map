4473bc7b6badc5d75e3e8e5a95c7f509a706bba3
lordslair <rvandepo+github@gmail.com>
2017-08-17 08:02:42 -0400

    Fixed path

--- docker/lib/UTM/Twitter.pm
+++ docker/lib/UTM/Twitter.pm
@@ -12,7 +12,7 @@ use YAML::Tiny;
 # Variables initialization
 #
 
-my $twitterfile = '../twitter-config.yaml';
+my $twitterfile = '/home/untappd-map/twitter-config.yaml';
 my $twittyaml = YAML::Tiny->read( $twitterfile );
 my $twitter = Net::Twitter::Lite::WithAPIv1_1->new(
     access_token_secret => $twittyaml->[0]{AccessTokenSecret},
