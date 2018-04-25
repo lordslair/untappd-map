24e02e4006779dc5feff74b4e8e12ee0552b6acf
lordslair <rvandepo+github@gmail.com>
2018-04-25 12:43:41 +0200

    Modified %Country for GB counting"

--- docker/lib/UTM/Untappd.pm
+++ docker/lib/UTM/Untappd.pm
@@ -47,6 +47,7 @@ sub getData
                 if ( $Country{'Scotland'} ) { $Country{'Great Britain'}{'count'} += $Country{'Scotland'}{'count'} ; delete $Country{'Scotland'} }
                 if ( $Country{'England'}  ) { $Country{'Great Britain'}{'count'} += $Country{'England'}{'count'}  ; delete $Country{'England'}  }
                 if ( $Country{'Wales'}    ) { $Country{'Great Britain'}{'count'} += $Country{'Wales'}{'count'}    ; delete $Country{'Wales'}    }
+               if ( $Country{'Northern Ireland'} ) { $Country{'Great Britain'}{'count'} += $Country{'Northern Ireland'}{'count'} ; delete $Country{'Northern Ireland'} }
              }
         }
         return \%Country;
