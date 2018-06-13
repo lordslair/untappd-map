f1325b553e5a795f2fff970cf4e7615ed375e0b5
lordslair <rvandepo+github@gmail.com>
2018-06-13 11:24:01 +0200

    Added test script for UTM::Twitter::SendTweet()

---
+++ docker/test/sendTweet.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Emoji::NationalFlag qw/ code2flag /;
+use utf8;
+use Data::Dumper;
+
+use lib '/home/untappd-map/lib';
+use UTM::Twitter;
+
+my $update_ref = UTM::Twitter::SendTweet( code2flag('jp') ) or die "Failed toSendTweet $!";
+my %update = %{$update_ref};
+
+if ($update{'id'})
+{
+    print "[$update{'id'}] Twitt sent\n";
+}
+else
+{
+   print "Oops! Something failed\n";
+}
