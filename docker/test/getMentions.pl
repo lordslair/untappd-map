a3c16121cd79cc299382efc3f5ab61817c4f477a
lordslair <5469390+lordslair@users.noreply.github.com>
2020-07-06 20:18:04 +0200

    Added wrap to ensure data exists

--- docker/test/getMentions.pl
+++ docker/test/getMentions.pl
@@ -16,9 +16,12 @@ my $Mentions = UTM::Twitter::getMentions;
 
 foreach my $id ( sort keys %{$Mentions} )
 {
-    my $okko;
-    my $username =  UTM::Twitter::SenderName($Mentions->{$id}{'sender_id'});
+    my $okko = ' ';
 
-    if ( $Mentions->{$id}{'replied'} ) { $okko = 'X' } else { $okko = ' ' }
-    printf "[%1s] | %-15d | %-15s | %-15s | %-15s | %-100s\n", $okko, $id, $Mentions->{$id}{'sender'}, $Mentions->{$id}{'sender_id'}, $username, $Mentions->{$id}{'text'};
+    if ($Mentions->{$id}{'sender_id'})
+    {
+        my $username =  UTM::Twitter::SenderName($Mentions->{$id}{'sender_id'});
+        if ( $Mentions->{$id}{'replied'} ) { $okko = 'X' }
+        printf "[%1s] | %-15d | %-15s | %-15s | %-15s | %-100s\n", $okko, $id, $Mentions->{$id}{'sender'}, $Mentions->{$id}{'sender_id'}, $username, $Mentions->{$id}{'text'};
+    }
 }
